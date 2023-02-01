828a5ed6b773d5886cd16bc340b48a78f101c655
drygin_alexey <drygin.alex@mail.ru>
2023-02-01 11:45:06 +0700

    create js Celsiy in Farengate

--- Sem3_HomeWork/html_1/1.js
+++ Sem3_HomeWork/html_1/1.js
@@ -1,3 +1,4 @@
 const a = Number.parseFloat(prompt("Введите температуру в Цельсиях"));
-const b = Number.parseInt((9 / 5) * a + 32);
+//const b = Number.parseInt((9 / 5) * a + 32);
+const b = parseFloat(((9 / 5) * a + 32).toFixed(1));
 alert(`Температура Цельсий ${a}, температура в фаренгейтах ${b}`);
